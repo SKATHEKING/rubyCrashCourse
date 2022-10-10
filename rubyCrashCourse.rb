b637d2a3de2e9fe257f40833f3e0049be43b50e0
SKATHEKING <mateusouro1@hotmail.com>
2022-10-10 14:46:51 +0100

    hashmaps and for loops added as well as case statements

--- rubyCrashCourse.rb
+++ rubyCrashCourse.rb
@@ -15,3 +15,64 @@ puts "enter your favourite food"
 #getting input
 food = gets.chomp()
 puts " your favourite food is " + food
+
+#arrays
+friends = Array["Tommy", "Kells" , "Pete", "Mac" , "50"]
+puts friend[0]
+puts friends[1,3]
+puts friends.length()
+puts friends.include? "Tommy"
+friends.reverse()
+friends.sort()
+
+for friend in friends
+  puts friend
+end
+
+for index in 0..4
+  puts friends[index]
+end
+
+if isMale
+  puts "your are male"
+else
+  puts "You must be a female"
+end
+
+#while loops
+index = 1
+while index <= 5
+  puts index
+  index += 1
+end
+
+#case statements
+case
+when 1
+  puts "one"
+when 2
+  puts "Two"
+when 3
+  puts "Three"
+end
+
+#hashmaps
+states = {
+  "Pennsylvania" => "PA",
+  "California" => "CA",
+  "New York" => "NY"
+}
+
+puts states["California"]
+
+def sayHi(name)
+  puts "Hello " + name
+end
+
+def returnAge(age)
+  return age
+end
+
+returnAge(age)
+
+sayHi("Mateus")
