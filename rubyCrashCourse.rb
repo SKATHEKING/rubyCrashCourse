808e46dda1597da1f7d02a73c978fc18d6de4948
SKATHEKING <mateusouro1@hotmail.com>
2022-10-10 11:04:05 +0100

    First few lines of code added

---
+++ rubyCrashCourse.rb
@@ -0,0 +1,12 @@
+
+#set up variables
+name = "Mateus"
+age = 24
+isMale = true
+flaws = nil
+
+#checks index of character within string
+puts name.index("M")
+
+puts "My name is " + name "and I am " + age + "years old."
+print name + " " + age
